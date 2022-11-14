428d261d73dbae1e41be5ae38dc67c3705847fc3
ebarnabo <edwinbarnabot@gmail.com>
2022-11-14 10:07:57 -0400

    Saisie

--- Kruskal.py
+++ Kruskal.py
@@ -13,3 +13,29 @@ Z = np.array([[0,1,0], [1,0,2], [0,0,0]])
 
 
 print(Z)
+
+# Algo de Kruskal
+# Exo 1 : 
+
+# Saisir une matrice et réordonné chaques arc par ordre croissant de poids
+
+# Function (X,Y) = Trie(Z)
+
+# Z est la matrice d'incidence concernée
+
+nbSommet = int(input ('Saisir le nombre de sommet :\n'))
+
+nbVal = nbSommet*nbSommet
+triPoids = []
+poids = []
+
+print('Saisir votre matrice :\n')
+i=0
+while (i<nbVal):
+    i+=1
+    for y in range (0,nbSommet-1):
+        x = int(input('Saisir votre sommet\n'))
+        if (x>0):
+            triPoids.append(x)
+            print (triPoids)
+            
