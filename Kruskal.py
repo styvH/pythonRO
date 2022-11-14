ab22fb53ed3bbabd02ade8878d70729494b58391
Styvan <styvan.h@gmail.com>
2022-11-14 09:56:27 -0400

    update model matrix

--- Kruskal.py
+++ Kruskal.py
@@ -2,9 +2,14 @@ import numpy as np
 
 
 # Entrer le nombre de sommets
-# Entrer la valeur de
 
-Z = np.array([[1,2,3], [4,5,6], [7,8,9]])
+# Tant que toutes les valeurs ne sont pas entrées
+
+# Demander la saisie pour la ligne concernée
+
+
+Z = np.array([[0,1,0], [1,0,2], [0,0,0]])
+
 
 
 print(Z)
