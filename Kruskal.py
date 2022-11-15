8d93c8f54aee3dbe8b9433547ad82dc51407d569
Styvan <styvan.h@gmail.com>
2022-11-15 14:37:59 -0400

    MAJ Trie Kruskal

--- Kruskal.py
+++ Kruskal.py
@@ -21,42 +21,7 @@ Z = np.array([[0,1,0], [1,0,2], [0,2,0]])
 
 # Function (X,Y) = Trie(Z)
 
-# Z est la matrice d'incidence concernée
 
-"""""
-nbSommet = int(input ('Saisir le nombre de sommet :\n'))
-
-nbVal = nbSommet*nbSommet
-triPoids = []
-Y = [] #poids
-X = [] #arcs
-i=0
-ligne=1
-print('Saisir votre matrice :\n')
-for x in range(0,nbVal):
-    print("x = "+str(x))
-    print("i = "+str(i))
-    if (i==nbSommet):
-        ligne+=1
-        i=0
-    print("Ligne : "+str(ligne)+"\nSommet: "+str(i+1))
-    sommetSaisi = int(input('Saisir votre sommet\n'))
-    if (sommetSaisi>0):
-        triPoids.append(sommetSaisi)
-        X.append(i)
-    else:
-        triPoids.append("L'arc n'existe pas")
-        X.append("L'arc n'existe pas")
-    i+=1
-print(triPoids)
-
-nbSommet = int(input ('Saisir le nombre de sommet :\n'))
-i = 0
-while (0 < nbSommet**2):
-    
-    a = int(input("Saisir valeur sommet"))
-    i+=1
-"""""
 #Ex 1 Trie Z
 def trie(Z):
 #Obtenir vecteur format 2 sorties : X = les chemins, Y = la taille
@@ -65,13 +30,16 @@ def trie(Z):
     print(nbCol)
     X = []
     Y = []
+    used = []
     for row in range(0,nbRow):
         for col in range(0,nbCol):
-            if Z[row,col] != 0:
+            if Z[row,col] != 0 and row not in used:
                 print("Ligne : ",row)
                 print("Colonne : ", col)
                 X += ["{}{}".format(row,col)]
                 Y += [Z[row,col]]
+                used+= [row]
+                used+=[col]
     render = "X = ", X,"Y = ",Y
 
     return render
