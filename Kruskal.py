6c0f26783e16ed69bd812c6bd3e91840010d55a6
Styvan <styvan.h@gmail.com>
2022-11-18 20:25:33 -0400

    update

--- Kruskal.py
+++ Kruskal.py
@@ -1,49 +1,48 @@
 import numpy as np
 
-
 # Entrer le nombre de sommets
 
 # Tant que toutes les valeurs ne sont pas entrées
 
 # Demander la saisie pour la ligne concernée
 
-Zbase = np.array([[0,5,0], [1,0,2], [0,2,0]])
+Zbase = np.array([[0, 5, 0], [1, 0, 2], [0, 2, 0]])
 
-Z = np.array([[0,7,0,5,0,0,0], [7,0,6,9,7,0,0], [0,6,0,0,4,0,0],[5,9,0,0,11,6,0],[0,7,4,11,0,8,9],[0,0,0,6,8,0,10],[0,0,0,0,9,10,0]])
+Z = np.array([[0, 7, 0, 5, 0, 0, 0], [7, 0, 6, 9, 7, 0, 0], [0, 6, 0, 0, 4, 0, 0], [5, 9, 0, 0, 11, 6, 0],
+              [0, 7, 4, 11, 0, 8, 9], [0, 0, 0, 6, 8, 0, 10], [0, 0, 0, 0, 9, 10, 0]])
 
 
-
-
-#print(Z)
+# print(Z)
 
 # Algo de Kruskal
 # Exo 1 : 
 
-# Saisir une matrice et réordonné chaques arc par ordre croissant de poids
+# Saisir une matrice et réordonné chaque arc par ordre croissant de poids
 
 # Function (X,Y) = Trie(Z)
 
 
-#Ex 1 Trie Z
-def trie(Z):#Obtenir vecteur format 2 sorties : X = les chemins, Y = la taille
-
-    nbRow, nbCol = Z.shape #Récupérer la taille de la matrice, soit le nombre de sommets
+# Ex 1 Trie Z
+def trie(Z):  # Obtenir vecteur format 2 sorties : X = les chemins, Y = la taille
+    nbRow, nbCol = Z.shape  # Récupérer la taille de la matrice, soit le nombre de sommets
     X = []
     Y = []
     used = []
     trie = []
     triearc = []
     i = 0
-    for row in range(0,nbRow):
-        for col in range(0,nbCol):
-            if Z[row,col] != 0 and col not in used: # Si la valeur n'est pas nulle, s'il y a un arc
-                if row != col: # Si sommet de départ n'est pas sommet d'arrivé
-                    X += ["{}{}".format(row,col)]
-                    Y += [Z[row,col]]
-                    used += [row]
+    for row in range(0, nbRow):  # pour chaque ligne
+        for col in range(0, nbCol):  # pour chaque colonne
+            if Z[row, col] != 0 and col not in used:  # Si la valeur n'est pas nulle, s'il y a un arc déjà existant
+                # (pour éviter les doublons)
+                if row != col:  # Si le sommet de départ n'est pas sommet d'arrivé
+                    X += ["{}{}".format(row, col)]  # Ajout de l'arc dans un tableau
+                    Y += [Z[row, col]]  # ajout de la taille correspondant à l'arc dans un deuxieme tableau
+                    used += [row]  # sauvegarder l'arc utilisé pour "éviter les doublons
+    # Démarrage du tri
     val = 0
     triesize = len(Y)
-    while ( len(trie) != triesize ):
+    while (len(trie) != triesize):
         i = 0
         valpos = 0
         for values in Y:
@@ -60,63 +59,23 @@ def trie(Z):#Obtenir vecteur format 2 sorties : X = les chemins, Y = la taille
         X.pop(valpos)
         Y.pop(valpos)
 
-
     X = triearc
     Y = trie
-    render = X,Y
-
+    render = X, Y
 
     return render
 
 
-
 print("Matrice : \n", Z)
-print("Taille : ",Z.shape)
+print("Taille : ", Z.shape)
 print(trie(Z))
 
 # Ex 2 vérifier pas de cycles
 
-#Zcycle = np.array([[0,7,0,5,0,0,0], [7,0,6,9,7,0,0], [0,6,0,0,4,0,0],[5,9,0,0,11,6,0],[0,7,4,11,0,8,9],[0,0,0,6,8,0,10],[0,0,0,0,9,10,0]])
-#print(Zcycle)
-#print(trie(Zcycle))
-
-
-# ACM ? Arbre couvrant minimum
-# Arbre qui passe par l'ensemble des sommets avec un poids minimum
-
-def ACM():
-
-    return 0
-
-
-import numpy as np
-
-
-# Entrer le nombre de sommets
-
-# Tant que toutes les valeurs ne sont pas entrées
-
-# Demander la saisie pour la ligne concernée
-
-
-
-#print(Z)
-
-# Algo de Kruskal
-# Exo 1 :
-
-# Saisir une matrice et réordonné chaques arc par ordre croissant de poids
-
-# Function (X,Y) = Trie(Z)
-
-
-#Ex 1 Trie Z
-
-
 
 print("Matrice : \n", Z)
-print("Taille : ",Z.shape)
-print(trie(Z))
+print("Taille : ", Z.shape)
+print(trie(Z))  # Affichage du tri
 
 X, Y = trie(Z)
 print("X = ", X)
@@ -124,57 +83,150 @@ print("Y = ", Y)
 
 # Ex 2 vérifier pas de cycles
 
-Zcycle = np.array([[0,1,1], [1,1,1],[1,1,0]])
+Zcycle = np.array([[0, 1, 1], [1, 1, 1], [1, 1, 0]])
 print(Zcycle)
 print(trie(Zcycle))
 print("X = ", X)
 print("Y = ", Y)
 
 
-def Kruskal(Z):
-    lesSommets = listeSommets(Z)
-    nbSommets = len(lesSommets)
-    #X, Y = trie(Z)
+def ExistChemin(MatriceAdjacente, posA, posB):  # s'il existe un chemin allant de A vers B, retourne vrai sinon faux
+    # Pour chaque sommet adjacent à 'A',
+    sAdj = [] # sAdj = liste des sommets adjacents pouvant créer un chemin
+    n = 0
+    for i in MatriceAdjacente[posA]:
 
-    poids = 0
-    return poids
+        if i != 0:  # Vérifier s'il existe nombre = 1 dans la ligne de 'A'
+
+            # Soit vérifier s'il existe un nombre = 1 dans la ligne de chaque ligne associé
+            sAdj.append(n)  # Récupérer les lignes associées (adjacentes) dans une liste
+
+        n += 1
+
+    # Pour chaque sommet adjacent à chaque sommet adjacent à 'a'
+    nbSommets = len(MatriceAdjacente[0])  # Nombre de Sommets
+    nbParcours = 1  # Nombre de sommets parcourus
+    while nbParcours < nbSommets:  # Tant que l'on n'a pas exécuté la boucle plus de fois que le nombre de sommets
+
+        for i in sAdj:
+            n = 0
+            for j in MatriceAdjacente[i]:
+                if n not in sAdj:
+                    if j != 0:  # Vérifier s'il existe nombre = 1 dans la ligne de 'A'
+                        sAdj.append(n)  # Récupérer les lignes associées (adjacents) dans une liste
+
+                n += 1
+        nbParcours += 1
+
+    # Vérifier si adjacent à 'B' (si = posB)
+
+    for adjacent in sAdj:
+        if adjacent == posB:
+            return True
+
+    return False
 
 
-print(trie(Z)[0][0])
-
-s0, s2 = trie(Z)[0][0]
-
-
-print(s2)
-
-def listeSommets(Z):
-    sommets = []
-    for i in range(len(trie(Z)[0])):
-        s1, s2 = trie(Z)[0][i]
-        if s1 not in sommets:
-            sommets += s1
-        if s2 not in sommets:
-            sommets += s2
-    return sommets
+def detectCycle(matrice):
+    for i in range(len(matrice)): #Pour chaque valeur sommet, vérifier s'il existe un cycle
+        if ExistChemin(matrice, int(i), int(i)):  # S'il existe un chemin allant de i vers i, il s'agit d'un cycle
+            return True
+    return False
 
 
-print(trie(Zbase))
-print(trie(Zbase)[0][0])
+# ACM : Arbre couvrant minimum
+# Arbre qui passe par l'ensemble des sommets à partir des poids minimum
 
-s0, s2 = trie(Zbase)[0][0]
-print(s2)
-
-print(listeSommets(Z))
-
-def delArcs(Z):
-    used = []
-    print(trie(Z))
+def ACPM(Z):
     arcs = []
-    arcs += trie(Z)[0][0],trie(Z)[1][0]
-    s1, s2 = trie(Z)[0][0]
-    used += s1
-    print(arcs)
-    #supprimer tout les s1 de Z
+    M = np.zeros((len(Z), len(Z)))  # Création matrice M nulle qui sera la matrice d'adjacente du graphe
+
+    # Démarrer boucle
+    for i in range(len(trie(Z)[0])):
+        x = trie(Z)[0][i]  # arc
+        y = trie(Z)[1][i]  # taille
+
+        s1, s2 = trie(Z)[0][i]
+        # Ajouter à la matrice d'adjacente
+        M[int(s1)][int(s2)] = 1
+        M[int(s2)][int(s1)] = 1
+        # Verifier cycle
+        if not detectCycle(M):  # S'il n'y a pas de cycle, on ajoute et on continue
+            arcs += [[x,y]]
+        else:   # S'il un cycle viens d'être ajouté, le retirer de la matrice d'adjacente
+            M[int(s1)][int(s2)] = 0
+            M[int(s2)][int(s1)] = 0
+
+    return arcs
 
 
-delArcs(Z)
+testCycle = np.array([[0, 3, 0, 0, 1], [3, 0, 5, 0, 4], [0, 5, 0, 2, 6], [0, 0, 2, 0, 7], [1, 4, 6, 7, 0]])
+
+print("_____________________________")
+
+print(testCycle)
+
+print("Trie : ", trie(testCycle))
+
+print(ACPM(testCycle))
+
+print("_____________________________")
+
+testCycle = np.array([[0, 2, 0, 0], [2, 0, 3, 0], [0, 0, 0, 1], [0, 3, 1, 0]])
+
+print(testCycle)
+print("Trie : ", trie(testCycle))
+print(ACPM(testCycle))
+
+print(Z)
+
+print(ACPM(Z))
+
+print("________________")
+
+
+
+print(ACPM(Z))
+print(trie(Z))
+print(Z[2][2])
+
+
+
+testCycle = np.array([[0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 1], [0, 3, 1, 0]])
+A = 0
+B = 1
+C = 2
+D = 3
+
+print(testCycle)
+
+
+
+print(ExistChemin(testCycle,A,D))
+
+
+print(ACPM(Z))
+print(trie(Z))
+
+
+testCycle = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+A = 0
+B = 1
+C = 2
+D = 3
+
+s1 = '03'
+
+a, b = s1
+
+print(int(a))
+
+testCycle[int(a)][int(b)] = 1
+testCycle[int(b)][int(a)] = 1
+
+testCycle[int(2)][int(1)] = 1
+testCycle[int(1)][int(2)] = 1
+print(testCycle)
+print(ExistChemin(testCycle,int(b),2))
+
+print(ACPM(Z))
